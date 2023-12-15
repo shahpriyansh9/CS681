a31e65602ad4b97e7224a9c1308893561105253a
shahpriyansh9 <115045289+shahpriyansh9@users.noreply.github.com>
2023-12-14 21:29:43 -0500

    Update TableObserver.java

--- HW_14/src/hw14/STOCK_APP/TableObserver.java
+++ HW_14/src/hw14/STOCK_APP/TableObserver.java
@@ -1,13 +1,3 @@
-// package hw14.STOCK_APP;
-
-// public class TableObserver implements Observer<StockEvent> {
-//     @Override
-//     public void update(Observable<StockEvent> sender, StockEvent event) {
-//         System.out.println("Table: " + event.ticker() + " @ " + event.quote());
-//     }
-// }
-
-
 package hw14.STOCK_APP;
 
 public class TableObserver implements Observer<StockEvent> {
