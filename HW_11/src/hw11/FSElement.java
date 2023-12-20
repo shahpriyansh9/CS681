4620eaaa0082f301736c450a952ae17452cedc30
shahpriyansh9 <115045289+shahpriyansh9@users.noreply.github.com>
2023-12-19 23:57:54 -0500

    Add files via upload

---
+++ HW_11/src/hw11/FSElement.java
@@ -0,0 +1,59 @@
+package hw11;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public abstract class FSElement {
+    protected Directory parent;
+    protected String name;
+    protected int size;
+    protected LocalDateTime creationTime;
+    private final Lock lock = new ReentrantLock();
+
+    public FSElement(Directory parent, String name, int size, LocalDateTime creationTime) {
+        this.parent = parent;
+        this.name = name;
+        this.size = size;
+        this.creationTime = creationTime;
+    }
+
+    public void setParent(Directory parent) {
+        lock.lock();
+        try {
+            this.parent = parent;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Directory getParent() {
+        return parent;
+    }
+
+    public void setName(String name) {
+        lock.lock();
+        try {
+            this.name = name;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public abstract boolean isDirectory();
+    public abstract boolean isFile();
+    public abstract boolean isLink();
+    public abstract void accept(FSVisitor v);
+}
