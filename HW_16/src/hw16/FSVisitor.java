12ece1fff59805756e7a88775b0090b52ebe9dad
shahpriyansh9 <115045289+shahpriyansh9@users.noreply.github.com>
2023-12-20 01:53:25 -0500

    Delete HW_16 directory

--- HW_16/src/hw16/FSVisitor.java
+++
@@ -1,9 +0,0 @@
-package hw16;
-
-public interface FSVisitor
-{
-     public abstract void visit(Link link);
-     public abstract void visit(Directory dir);
-     public abstract void visit(File file);
-
-}
