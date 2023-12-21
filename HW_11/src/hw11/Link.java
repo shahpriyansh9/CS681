991db07759abbc0ad9af61aeb6b3150b68666638
shahpriyansh9 <115045289+shahpriyansh9@users.noreply.github.com>
2023-12-21 18:50:03 -0500

    Add files via upload

---
+++ HW_11/src/hw11/Link.java
@@ -0,0 +1,44 @@
+package hw11;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Link extends FSElement {
+    private FSElement target;
+    private final Lock lock = new ReentrantLock();
+
+    public Link(Directory parent, String name, int size, LocalDateTime creDateTime, FSElement target) {
+        super(parent, name, size, creDateTime);
+        this.target = target;
+    }
+
+    public boolean isDirectory() {
+        return false;
+    }
+
+    public boolean isFile() {
+        return false;
+    }
+
+    public boolean isLink() {
+        return true;
+    }
+
+    public void setTarget(FSElement target) {
+        this.target = target;
+    }
+
+    public FSElement getTarget() {
+        return target;
+    }
+
+    public void accept(FSVisitor v) {
+        lock.lock();
+        try {
+            v.visit(this);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
