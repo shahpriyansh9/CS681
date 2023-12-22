8a63833dc2d40b376b0de7d9bdc2ce26fca91f93
shahpriyansh9 <115045289+shahpriyansh9@users.noreply.github.com>
2023-12-21 19:04:21 -0500

    Update FileCrawlingVisitor.java

--- HW_16/src/hw16/util/FileCrawlingVisitor.java
+++ HW_16/src/hw16/util/FileCrawlingVisitor.java
@@ -13,11 +13,11 @@ public class FileCrawlingVisitor implements FSVisitor {
     private List<File> files = new CopyOnWriteArrayList<>();
 
     public void visit(Link link) {
-        // Implementation if needed
+        
     }
 
     public void visit(Directory dir) {
-        // Implementation if needed
+       
     }
 
     public void visit(File file) {
@@ -25,10 +25,10 @@ public class FileCrawlingVisitor implements FSVisitor {
     }
 
     public List<File> getFiles() {
-        return new ArrayList<>(files); // Return a copy of the files
+        return new ArrayList<>(files); 
     }
 
     public void clearFiles() {
-        files.clear(); // Clears the internal list of files
+        files.clear(); 
     }
 }
